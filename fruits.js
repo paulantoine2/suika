d7924ff585e97b580bb98a4b972717c6e192c7b2
Paul ANTOINE <paul.antoine2@gmail.com>
2023-10-26 00:32:11 +0200

    Innitial commit

---
+++ fruits.js
@@ -0,0 +1,57 @@
+export const FRUITS = [
+  {
+    label: "cherry",
+    radius: 40 / 2,
+    color: "#F20306",
+  },
+  {
+    label: "strawberry",
+    radius: 50 / 2,
+    color: "#FF624C",
+  },
+  {
+    label: "grape",
+    radius: 72 / 2,
+    color: "#A969FF",
+  },
+  {
+    label: "kumkwat",
+    radius: 85 / 2,
+    color: "#FFAF02",
+  },
+  {
+    label: "orange",
+    radius: 106 / 2,
+    color: "#FC8611",
+  },
+  {
+    label: "apple",
+    radius: 140 / 2,
+    color: "#F41615",
+  },
+  {
+    label: "pear",
+    radius: 160 / 2,
+    color: "#FDF176",
+  },
+  {
+    label: "peach",
+    radius: 196 / 2,
+    color: "#FEB6AC",
+  },
+  {
+    label: "pineapple",
+    radius: 220 / 2,
+    color: "#F7E608",
+  },
+  {
+    label: "melon",
+    radius: 270 / 2,
+    color: "#89CE13",
+  },
+  {
+    label: "watermelon",
+    radius: 300 / 2,
+    color: "#26AA1E",
+  },
+];
